fd8cb9317e851e4463df6d4d238e67a10e907b5f
Your Name <you@example.com>
2019-10-21 13:35:24 -0600

    initial commit

---
+++ src/PalTracker/EnvController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PalTracker
+{
+    public class EnvController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
